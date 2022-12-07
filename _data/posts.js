84d07726bd7d3910abffad577b2deb78b0655f80
Bastian Allgeier <mail@bastianallgeier.com>
2022-12-07 16:32:43 +0100

    Smaller changes

--- _data/posts.js
+++ _data/posts.js
@@ -1,19 +1,22 @@
-const axios = require('axios')
+const { $fetch } = require("ofetch");
 
 module.exports = async function () {
 
-    const api = 'https://kql.getkirby.com/api/query';
+    const api = "https://kql.getkirby.com/api/query";
 
-    const response = await axios.post(api, {
-        query: 'page("notes").children',
-        select: {
-            title: true,
-            text: 'page.text.kirbytext',
-            slug: true,
-            date: 'page.date.todate("d.m.Y")'
+    const response = await $fetch(api, {
+        method: "post",
+        body: {
+            query: "page('notes').children.sortBy('date', 'desc')",
+            select: {
+                title: true,
+                text: "page.text.toBlocks.toHtml",
+                slug: true,
+                date: "page.date.toDate('d.m.Y')"
+            }
         }
     });
 
-    return response.data.result.data;
+    return response.result;
 
 }
